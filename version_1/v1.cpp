8d1efed39f4cf89c82bc28e43d2fb4dba11ca939
Aitortazo <aitortazzo@gmail.com>
2021-11-15 19:43:49 +0100

    v1 codigo actualizado

--- version_1/v1.cpp
+++ version_1/v1.cpp
@@ -3,38 +3,113 @@
 #include <ctime>
 using namespace std;
 
-#define CYN "\e[0;36m"
-#define NC "\e[0m"
-#define green "\u001b[32;1m"
-#define BRed "\u001b[41m"
-#define BBlack "\u001b[40m"
-#define BWhite "\u001b[47m"
-#define underline "\033[4m"
-#define normal "\033[0m"
+const string CYN = "\e[0;36m";
+const string NC = "\e[0m";
+const string green = "\u001b[32;1m";
+const string BRed = "\u001b[41m";
+const string BBlack = "\u001b[40m";
+const string BWhite = "\u001b[47m";
+const string underline = "\033[4m";
+const string normal = "\033[0m";
 
 const bool DEBUG_MODE = true, JUEGO_ALEATORIO = true;
 const int ANCHO_PISTA = 7, MIN_HABILIDAD = 1, MAX_HABILIDAD = 3, MIN_VELOCIDAD = 1, MAX_VELOCIDAD = 5;
 
+string marcador(int puntuacion);
+int correTenista(int posicion_tenista, int velocidad, int posicion_bola);
+int introducirDato(string dato, int min_dato, int max_dato);
+int golpeoBola(int posicion_tenista, int habilidad);
+string saqueInicial(string nombre1, string nombre2);
+string juego(int habilidad1, int habilidad2, int velocidad1, int velocidad2, string nombre1, string nombre2);
+
+
+int main()
+{
+    srand(time(NULL));
+    string nombre1, nombre2, ganador_punto;
+    int habilidad1, habilidad2, velocidad1, velocidad2, punt1 = 0, punt2 = 0;
+    bool ganador;
+
+    cout << underline << BRed << "BIENVENIDO/S AL MEJOR TENIS JAMAS PROGRAMADO" << BBlack << normal << endl;
+    cout << " " << endl;
+    cout << green << R"(
+    ,odOO"bo,
+  ,dOOOP'dOOOb,
+ ,O3OP'dOO3OO33,
+ P",ad33O333O3Ob
+ ?833O338333P",d
+ `88383838P,d38'
+  `Y8888P,d88P'
+    `"?8,8P"'
+    )" << NC << '\n';
+    //Datos tenista 1
+    cout << endl << "Datos del tenista 1" << endl;
+    cout << "- Iniciales del tenista1: ";
+    cin >> nombre1;
+    habilidad1 = introducirDato("habilidad", MIN_HABILIDAD, MAX_HABILIDAD);
+    velocidad1 = introducirDato("velocidad", MIN_VELOCIDAD, MAX_VELOCIDAD);
+
+    //Datos tenista 2
+    cout << endl << "Datos del tenista 2" << endl;
+    cout << "- Iniciales del tenista2: ";
+    cin >> nombre2;
+    habilidad2 = introducirDato("habilidad", MIN_HABILIDAD, MAX_HABILIDAD);
+    velocidad2 = introducirDato("velocidad", MIN_VELOCIDAD, MAX_VELOCIDAD);
+
+    //Juego
+    ganador = false;
+
+    while(!ganador){
+        cout << CYN << marcador(punt1) << " " << nombre1 << " - " << nombre2 << " " << marcador(punt2) << NC << endl;
+        if(punt1 > 3 && punt2 < 3){
+            cout << "El ganador del juego es " << nombre1 << "!!" << endl;
+            ganador = true;
+        }else if(punt2 > 3 && punt1 < 3){
+            cout << "El ganador del juego es " << nombre2 << "!!" << endl;
+            ganador = true;
+        }else if(punt1 == 3 && punt2 == 5 ){
+            cout << "El ganador del juego es " << nombre2 << "!!" << endl;
+            ganador = true;
+        } else if ( punt2 == 3 && punt1 == 5 ){
+            cout << "El ganador del juego es " << nombre1 << "!!" << endl;
+            ganador = true;
+        } else {
+            if(juego(habilidad1, habilidad2, velocidad1, velocidad2, nombre1, nombre2) == nombre1){
+                punt1++;
+            }else{
+                punt2++;
+            }
+        }
+        if(punt1 == punt2 && punt1 == 4){
+                punt1 = 3;
+                punt2 = 3;
+        }
+    }
+    return 0;
+}
+
 string marcador(int puntuacion)
 {
+    string punt;
     switch(puntuacion)
     {
     case 0:
-        return "00";
+        punt = "00";
     break;
     case 1:
-        return "15";
+        punt = "15";
     break;
     case 2:
-        return "30";
+        punt = "30";
     break;
     case 3:
-        return "40";
+        punt = "40";
     break;
     case 4:
-        return "Ad";
+        punt = "Ad";
     break;
     }
+    return punt;
 }
 
 int correTenista(int posicion_tenista, int velocidad, int posicion_bola)
@@ -179,67 +254,3 @@ string juego(int habilidad1, int habilidad2, int velocidad1, int velocidad2, str
 }
 
 
-int main()
-{
-    srand(time(NULL));
-    string nombre1, nombre2, ganador_punto;
-    int habilidad1, habilidad2, velocidad1, velocidad2, punt1 = 0, punt2 = 0;
-    bool ganador;
-
-    cout << underline << BRed << "BIENVENIDO/S AL MEJOR TENIS JAMAS PROGRAMADO" << BBlack << normal << endl;
-    cout << " " << endl;
-    cout << green << R"(
-    ,odOO"bo,
-  ,dOOOP'dOOOb,
- ,O3OP'dOO3OO33,
- P",ad33O333O3Ob
- ?833O338333P",d
- `88383838P,d38'
-  `Y8888P,d88P'
-    `"?8,8P"'
-    )" << NC << '\n';
-    //Datos tenista 1
-    cout << endl << "Datos del tenista 1" << endl;
-    cout << "- Iniciales del tenista1: ";
-    cin >> nombre1;
-    habilidad1 = introducirDato("habilidad", MIN_HABILIDAD, MAX_HABILIDAD);
-    velocidad1 = introducirDato("velocidad", MIN_VELOCIDAD, MAX_VELOCIDAD);
-
-    //Datos tenista 2
-    cout << endl << "Datos del tenista 2" << endl;
-    cout << "- Iniciales del tenista2: ";
-    cin >> nombre2;
-    habilidad2 = introducirDato("habilidad", MIN_HABILIDAD, MAX_HABILIDAD);
-    velocidad2 = introducirDato("velocidad", MIN_VELOCIDAD, MAX_VELOCIDAD);
-
-    //Juego
-    ganador = false;
-
-    while(!ganador){
-        cout << CYN << marcador(punt1) << " " << nombre1 << " - " << nombre2 << " " << marcador(punt2) << NC << endl;
-        if(punt1 > 3 && punt2 < 3){
-            cout << "El ganador del juego es " << nombre1 << "!!" << endl;
-            ganador = true;
-        }else if(punt2 > 3 && punt1 < 3){
-            cout << "El ganador del juego es " << nombre2 << "!!" << endl;
-            ganador = true;
-        }else if(punt1 == 3 && punt2 == 5 ){
-            cout << "El ganador del juego es " << nombre2 << "!!" << endl;
-            ganador = true;
-        } else if ( punt2 == 3 && punt1 == 5 ){
-            cout << "El ganador del juego es " << nombre1 << "!!" << endl;
-            ganador = true;
-        } else {
-            if(juego(habilidad1, habilidad2, velocidad1, velocidad2, nombre1, nombre2) == nombre1){
-                punt1++;
-            }else{
-                punt2++;
-            }
-        }
-        if(punt1 == punt2 && punt1 == 4){
-                punt1 = 3;
-                punt2 = 3;
-        }
-    }
-    return 0;
-}
